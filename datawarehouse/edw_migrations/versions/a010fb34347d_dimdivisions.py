a113292013f4747da849f9f51c50302f08ae38a7
Abin Antony <abin.antony@aot-technologies.com>
2022-06-24 11:17:14 -0700

    #2374 - divisions Dim tables

---
+++ datawarehouse/edw_migrations/versions/a010fb34347d_dimdivisions.py
@@ -0,0 +1,30 @@
+"""empty message
+
+Revision ID: a010fb34347d
+Revises: e6460d4979c4
+Create Date: 2022-06-24 10:50:20.543192
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a010fb34347d'
+down_revision = 'e6460d4979c4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('dimDivisions',
+		sa.Column('divisionid', sa.Integer(), nullable=False),
+		sa.Column('division', sa.VARCHAR(length=500)),
+		sa.Column('description', sa.TEXT),
+		sa.Column('cactive', sa.CHAR(length=1)),
+		sa.PrimaryKeyConstraint('divisionid')
+    )
+
+
+def downgrade():
+    op.drop_table('dimDivisions')
