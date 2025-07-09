bfb37ddad766816d0de03921b1871e2e411802ce
nkan-aot2 <nkan-aot2>
2025-07-09 22:15:37 +0000

    add additional sql files

---
+++ databricks/sqlupdates/add_sourceoftruth_column.sql
@@ -0,0 +1,34 @@
+%sql
+
+alter table factrequestcustomcalcfields ADD COLUMNS (
+    sourceoftruth string
+);
+
+update factrequestcustomcalcfields set sourceoftruth = 'AXIS';
+
+alter table factRequestExtensions ADD COLUMNS (
+    sourceoftruth string
+);
+
+update factRequestExtensions set sourceoftruth = 'AXIS';
+
+alter table factrequestfordocuments ADD COLUMNS (
+    sourceoftruth string
+);
+
+update factrequestfordocuments set sourceoftruth = 'AXIS';
+
+alter table factrequestpaymenttransaction ADD COLUMNS (
+    sourceoftruth string
+);
+
+update factrequestpaymenttransaction set sourceoftruth = 'AXIS';
+
+alter table factrequestinvoices ADD COLUMNS (
+    sourceoftruth string
+);
+
+update factrequestinvoices set sourceoftruth = 'AXIS';
+
+
+
