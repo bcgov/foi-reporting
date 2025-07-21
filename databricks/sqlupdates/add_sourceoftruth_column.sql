fde1fd3d879d6404dc8b10dcac888499bada9c50
nkan-aot2 <nkan-aot2>
2025-07-21 18:25:23 +0000

    combine sql file and remove dimrequesters as it already has source of truth

--- databricks/sqlupdates/add_sourceoftruth_column.sql
+++ databricks/sqlupdates/add_sourceoftruth_column.sql
@@ -1,5 +1,77 @@
 %sql
 
+%sql
+
+alter table dimaddress ADD COLUMNS (
+    sourceoftruth string
+);
+update dimaddress set sourceoftruth = 'AXIS';
+
+alter table dimdeliverymodes ADD COLUMNS (
+    sourceoftruth string
+);
+update dimdeliverymodes set sourceoftruth = 'AXIS';
+
+alter table dimdivisions ADD COLUMNS (
+    sourceoftruth string
+);
+update dimdivisions set sourceoftruth = 'AXIS';
+
+alter table dimecgroups ADD COLUMNS (
+    sourceoftruth string
+);
+update dimecgroups set sourceoftruth = 'AXIS';
+
+alter table dimextensiontypes ADD COLUMNS (
+    sourceoftruth string
+);
+update dimextensiontypes set sourceoftruth = 'AXIS';
+
+alter table dimfoiflowrequeststatus ADD COLUMNS (
+    sourceoftruth string
+);
+update dimfoiflowrequeststatus set sourceoftruth = 'AXIS';
+
+alter table diminvoicedetails ADD COLUMNS (
+    sourceoftruth string
+);
+update diminvoicedetails set sourceoftruth = 'AXIS';
+
+alter table dimmodministryrequestids ADD COLUMNS (
+    sourceoftruth string
+);
+update dimmodministryrequestids set sourceoftruth = 'AXIS';
+
+alter table dimmodpageflags ADD COLUMNS (
+    sourceoftruth string
+);
+update dimmodpageflags set sourceoftruth = 'AXIS';
+
+alter table dimreceivedmodes ADD COLUMNS (
+    sourceoftruth string
+);
+update dimreceivedmodes set sourceoftruth = 'AXIS';
+
+alter table dimrequestertypes ADD COLUMNS (
+    sourceoftruth string
+);
+update dimrequestertypes set sourceoftruth = 'AXIS';
+
+alter table dimrequestfordocumentsstatus ADD COLUMNS (
+    sourceoftruth string
+);
+update dimrequestfordocumentsstatus set sourceoftruth = 'AXIS';
+
+alter table dimrequeststatuses ADD COLUMNS (
+    sourceoftruth string
+);
+update dimrequeststatuses set sourceoftruth = 'AXIS';
+
+alter table dimstages ADD COLUMNS (
+    sourceoftruth string
+);
+update dimstages set sourceoftruth = 'AXIS';
+
 alter table factrequestcustomcalcfields ADD COLUMNS (
     sourceoftruth string
 );
